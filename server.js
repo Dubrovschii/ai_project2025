afe83efd6acbdd4a00f7284bcc98c6b1ef4fb7f0
Alexei <dubrovschii@gmail.com>
2025-01-09 11:21:23 +0200

    add change

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
+app.use('/', express.static('public'))
+
+app.get('/api/test', (req, res) => {
+    res.json({
+        "sallary": 500
+    })
+})
 app.get('/', (req, res) => {
-    res.send('Hello World213!');
-});
+    res.send('Hello World213!')
+})
 
-module.exports = app;
+app.listen(port, () => {
+    console.log(`Example app 1listening on port ${port}`)
+})
